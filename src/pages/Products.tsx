a91cf0a282fac75d380524ade348f07a99cb1193
andile987 <141252573+andile987@users.noreply.github.com>
2026-02-11 23:16:29 +0000

    products_single_vip_toilet

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -47,7 +47,7 @@ const Products = () => {
           id: "standard-portable",
           name: "Single Vip Toilet",
           image: "https://scontent.fjnb1-1.fna.fbcdn.net/v/t39.30808-6/633857073_1401276962013787_4223166535954921383_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGuB4WNB7HS-zkpx5VEw_5kRnOTUlt-3-xGc5NSW37f7Lz5dyd0enI0nkjMqNQxl4TYCxjW--2MgmmZmta6efnh&_nc_ohc=ePFPkaDXWWoQ7kNvwEeq88j&_nc_oc=Adn0-I_C1QjIJJIt-pU_GhIsO-LnpIUcflvNWGDu7k09zJSKUEbdPpasBF44MDjZd3Q&_nc_zt=23&_nc_ht=scontent.fjnb1-1.fna&_nc_gid=hA2IYcNLfnDS5F8pQDGHKA&oh=00_Afu5gKXPewDqjFFmIsehMrj1DxNPTWQZ4Eas38-pewmhuA&oe=6992BBA7",
-          price: 3999,99,
+          price: 39 999,99,
           rating: 4.7,
           category: "toilets",
           description: "Durable and reliable standard portable toilet for any setting.",
